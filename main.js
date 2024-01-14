db2e7f91454822b28fb22097a63da9035c3249d5
EslamMohamed2025 <152355193+EslamMohamed2025@users.noreply.github.com>
2024-01-14 22:29:30 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+document.querySelector("nav .menu").onclick = function () {
+document.querySelector("ul").classList.toggle(`open`);
+}
+
+document.querySelector("nav .moon").onclick = function () {
+    document.querySelector("body").classList.toggle(`non`);
+    document.querySelector("i").classList.toggle(`nin`);
+    document.querySelector("nav").classList.toggle(`non`);
+    document.querySelector("nav a").classList.toggle(`non`);
+    document.querySelector("header h2").classList.toggle(`nmn`);
+    document.querySelector("header span").classList.toggle(`non`);
+
+    }
+
+    
